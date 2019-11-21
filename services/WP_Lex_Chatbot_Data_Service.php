69249ce62e71fc9481d47cf933bd3836fc989b0b
Jeff Everhart <Jeffeverhart383@gmail.com>
2019-11-21 13:05:10 -0500

    get PHP DataService working to initialize table and write messages

--- services/WP_Lex_Chatbot_Data_Service.php
+++ services/WP_Lex_Chatbot_Data_Service.php
@@ -1,34 +1,49 @@
 <?php
-
 class WP_Lex_Chatbot_Data_Service {
-  private $conversation_table;
-  private $charset_collate;
+  private static $conversation_table = 'wp_lex_chatbot_messages';
   function __construct() {
-    global $wpdb;
-    $this->conversation_table = $wpdb->prefix . 'wp_lex_chatbot_messages';
-    $this->charset_collate = $wpdb->get_charset_collate();
-  }
-  public function register_database_tables ($file) {
-    register_activation_hook($file, array($this, 'initialize_plugin_tables'));
-  }
-  public function initialize_plugin_tables () {
-    global $wpdb;
 
-    $sql = "CREATE TABLE" .$this->conversation_table."(
+  }
+
+  public static function initialize_plugin_tables () {
+    global $wpdb;
+    $table_name = $wpdb->prefix . self::$conversation_table;
+    $sql = "CREATE TABLE " . $table_name . " (
           id mediumint(9) NOT NULL AUTO_INCREMENT,
           conversation_id bigint NOT NULL,
-          speaker VARCHAR NOT NULL,
-          utterance VARCHAR NOT NULL,
-          intent,
+          speaker VARCHAR(255) NOT NULL,
+          utterance VARCHAR(255) NOT NULL,
+          intent VARCHAR(255),
+          PRIMARY KEY(id)
 
-    )". $this->charset_collate.";";
+    )". $wpdb->get_charset_collate().";";
 
     require_once(ABSPATH. 'wp-admin/includes/upgrade.php');
     dbDelta($sql);
   }
 
-  public function add_message_to_database ($message) {
+  public static function add_message_to_database ($message) {
+    global $wpdb;
+    // Replace with WPDB prepare: https://developer.wordpress.org/reference/classes/wpdb/prepare/
+    $table_name = $wpdb->prefix . self::$conversation_table;
+    $id = 9999;
+    $speaker = "'jeff'";
+    $utterance = "'Here are some other words'";
+    $intent = "'Here is some other intent'";
+    $sql = "INSERT INTO " . $table_name . " (
+          conversation_id,
+          speaker,
+          utterance,
+          intent
+        ) VALUES (
+          {$id},
+          {$speaker},
+          {$utterance},
+          {$intent}
+        );";
 
+    require_once(ABSPATH. 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
   }
 
   public function get_messages_from_database () {
